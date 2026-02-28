518e52e5cd3a34455f593272cb30b5b59f208afb
Nico <josenicolasbarrosogarcia@gmail.com>
2026-02-28 22:22:49 +0000

    fix(data): fixed no empty json parsed if there's an error

--- app/data/data_handler.py
+++ app/data/data_handler.py
@@ -1,19 +1,22 @@
 import json
 import os
+from functools import partial
 
 
 def load_data(file_path: str):
     """
-    file_path(String): defines the JSON file path to be read.
+    file_path(String):reads the JSON file from the path provided.
     """
     try:
-        with open(file_path, "r") as f:
-            data = json.load(f)
+        with open(file_path, "r") as file:
+            data = json.load(file)
         return data
-    except FileNotFoundError as e:
-        print(f"Error. File not found", e)
-    except json.JSONDecodeError as e:
-        print(f"There's a fortmar error in the file", e)      
+    except FileNotFoundError as error:
+        print(f"Error. File not found", error)
+        return []
+    except json.JSONDecodeError as error:
+        print(f"There's a format error in the file", error)    
+        return []  
 
 
-load_tasks = lambda: load_data("data/tasks.json")
+load_tasks = partial(load_data, "data/tasks.json")
